3500e8af5cd719cbcebd0ea7d88b8fdbc6577c08
Safral <syedsafral@gmail.com>
2023-08-15 16:18:15 +0530

    first commit

---
+++ resources/views/head/head.blade.php
@@ -0,0 +1,28 @@
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+
+<!-- Template Main CSS File -->
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+
+
+<!-- Vendor JS Files -->
+<script src="{{asset('vendor/purecounter/purecounter_vanilla.js')}}"></script>
+<script src="{{asset('vendor/aos/aos.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('vendor/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('vendor/waypoints/noframework.waypoints.js')}}"></script>
+<script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+
+<script src="{{asset('js/main.js')}}"></script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>BizLand Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  
+
+  <!-- =======================================================
+  * Template Name: BizLand
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  @include('inc.navbar')
+
+  @yield('content')
+  @include('inc.footer')
+
+  </main><!-- End #main -->
+
+  
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  @include('head.head')
+
+</body>
+
+</html>
